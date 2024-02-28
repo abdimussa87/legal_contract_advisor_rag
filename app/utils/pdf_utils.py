9bb78dd5a74a72bd8ffa41bf07f6b107d92678c3
Abdi Mussa <abdimussa87@gmail.com>
2024-02-28 11:34:02 +0300

    fix: pdf now returned as documents instead of single text

--- app/utils/pdf_utils.py
+++ app/utils/pdf_utils.py
@@ -1,14 +1,22 @@
 from PyPDF2 import PdfReader
+from langchain.docstore.document import Document
 
 
 class MyPDF:
     def __init__(self, pdf):
         self.pdf = pdf
 
-    def get_pdf_text(self):
-        text = ""
+    def get_pdf_docs(self):
+        docs = []
         for pdf in self.pdf:
             pdf_reader = PdfReader(pdf)
+            i = 1
             for page in pdf_reader.pages:
-                text += page.extract_text()
-        return text
+                docs.append(
+                    Document(
+                        page_content=page.extract_text().replace("\n", " "),
+                        metadata={"page": i},
+                    )
+                )
+                i += 1
+        return docs
